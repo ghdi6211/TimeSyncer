517af974d40e41c79aa48cd71a3ee986350ca2f0
dongyeon <ehddus2738@naver.com>
2023-09-18 19:50:32 +0900

    cors대응 및 login화면 response 확인

--- server/server.py
+++ server/server.py
@@ -1,11 +1,18 @@
 from flask import Flask
 from flask import request, make_response, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app, resources={r'*':{'origins':'http://localhost:3000'}},supports_credentials=True)
 
 @app.route("/", methods=['GET'])
 def index():
     return "Hello World!!!"
+
+@app.route("/user/login", methods=['POST'])
+def login():
+    return "loginTest!!!"
 
 if __name__ == "__main__":
     app.debug = True
